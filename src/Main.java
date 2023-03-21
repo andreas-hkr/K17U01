7524607c17a9fa0b5d70872ce6ba09cfc67756d8
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-21 11:17:13 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skapa några klasser som alla representerar djur, låt dem alla ärva från en gemensam klass Animal
+        //  - Animal skall ha en abstrakt metod makeSound()
+        //  - Animal <-- ( Mammal / Reptil ) <-- Dog / Snake
+        //
+        // Skapa en array med djur-objekt i, loopa igenom alla djur och kalla på deras makeSound-metod
+
+
+    }
+}
